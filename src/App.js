6fe6d34c2945f87838ac32c7ba28e82fb903a8dd
DimasFrans14 <dimasfranss@gmail.com>
2022-11-20 18:21:42 +0700

    update di app js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,48 @@
 // Import Component
-import NavbarComp from "./Component/Navbar/NavbarComp"
+import NavbarComp from "./Component/Navbar/NavbarComp";
 import Content1 from "./Component/Content/Content1";
 import Content2 from "./Component/Content/Content2";
 import Content3 from "./Component/Content/Content3";
 import Content4 from "./Component/Content/Content4";
 import Footer from "./Component/Footer/Footer";
 
-import { useEffect } from 'react';
+// Library Loader
+import HashLoader from "react-spinners/HashLoader";
+
+import { useEffect, useState } from "react";
 
 function App() {
+  const [isLoading, setLoading] = useState(false);
 
   useEffect(() => {
-    document.title = 'Porto - Dimas Frans S';
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 2500);
+  }, []);
+
+  useEffect(() => {
+    document.title = "Porto - Dimas Frans S";
   }, []);
 
   return (
     <>
-        <NavbarComp/>
-        <Content1/>
-        <Content2/>
-        <Content3/>
-        <Content4/>
-        <Footer/>
+      <div className="App">
+        {isLoading ? (
+          <HashLoader color="#36d7b7"
+            className="page-loader"
+          />
+        ) : (
+          <>
+            <NavbarComp />
+            <Content1 />
+            <Content2 />
+            <Content3 />
+            <Content4 />
+            <Footer />
+          </>
+        )}
+      </div>
     </>
   );
 }
